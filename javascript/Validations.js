ed80419b4cc086520a7fc23ef19f703ef7f40e9b
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2017-05-29 00:29:29 +0530

    sign up finalized
    have to do the validations

---
+++ javascript/Validations.js
@@ -0,0 +1,18 @@
+var fname = document.getElementById('firstName');
+var lname = document.getElementById('lastName');
+var useraddress = document.getElementById('address');
+var userbday = document.getElementById('bDay');
+var usermail = document.getElementById('email');
+var tel= document.getElementById('telephoneNo');
+var pass= document.getElementById('password');
+var cmfpass =  document.getElementById('confirmPassword');
+var agree = document.getElementById('agreement');
+var gender = document.getElementById('gender');
+
+function submitOnclick(){
+    if(true){
+        document.getElementById('id01').style.display='block';
+    }
+
+}
+
--- javascript/styleThings.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by user on 5/28/2017.
- */
-var indexwindow = document.getElementById("indexContainer")
-function showIndexWindow(){
-    indexwindow.style.
-}
