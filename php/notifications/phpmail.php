15433f3d8037ab3a147eb222ce903292c66a9a18
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2017-07-10 02:41:01 +0530

    Official mail detail added

--- php/notifications/phpmail.php
+++ php/notifications/phpmail.php
@@ -7,13 +7,13 @@ function sendMail($email,$subject, $bodyContent, $sender)
     $mail->isSMTP();                                   // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';                    // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                            // Enable SMTP authentication
-    $mail->Username = 'yurekatest123@gmail.com';          // SMTP username
-    $mail->Password = 'yureka123456789';            // SMTP password
+    $mail->Username = 'yurekahighereducation@gmail.com';          // SMTP username
+    $mail->Password = 'yurekaofficial';            // SMTP password
     $mail->SMTPSecure = 'tls';                         // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                 // TCP port to connect to
 
-    $mail->setFrom('yurekatest234@gmail.com', 'Yureka');
-    $mail->addReplyTo('yurekatest234@gmail.com', 'Yureka');
+    $mail->setFrom('yurekahighereducation@gmail.com', 'Yureka Institute');
+    $mail->addReplyTo('yurekahighereducation@gmail.com', 'Yureka Institute');
     $mail->addAddress($email);   // Add a recipient
     //$mail->addCC('cc@example.com');
     //$mail->addBCC('bcc@example.com');
